a74652e80ca404750034f7061944848f95135382
Yogesh Jajoria <yogeshjajoria2019@gmail.com>
2024-07-08 23:54:00 +0530

    navbar basic setup

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,16 @@
+import Links from "./links/Links";
+
+const Navbar = () => {
+  return (
+    <div>
+      <div>Logo</div>
+      <div>
+        <div>
+          <Links />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ src/components/navbar/links/Links.jsx
@@ -0,0 +1,23 @@
+import Link from "next/link";
+
+const Links = () => {
+  const links = [
+    { title: "Home", path: "/" },
+    { title: "About", path: "/about" },
+    { title: "Contact", path: "/contact" },
+    { title: "Blog", path: "/blog" },
+  ];
+  return (
+    <div>
+      {links.map((link) => {
+        return (
+          <Link href={link.path} key={link.title}>
+            {link.title}
+          </Link>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Links;
