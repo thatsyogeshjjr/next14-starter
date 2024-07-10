2bc440492a3d57091741a2d6f767656b5b0fce46
Yogesh Jajoria <yogeshjajoria2019@gmail.com>
2024-07-10 23:13:47 +0530

    added responsive navbar

--- src/components/navbar/links/Links.jsx
+++ src/components/navbar/links/Links.jsx
@@ -1,8 +1,10 @@
-import Link from "next/link";
+"use client";
 import styles from "./links.module.css";
 import NavLink from "./navLink/NavLink";
+import { useState } from "react";
 
 const Links = () => {
+  const [open, setOpen] = useState(false);
   const links = [
     { title: "Home", path: "/" },
     { title: "About", path: "/about" },
@@ -15,24 +17,46 @@ const Links = () => {
   const isAdmin = true;
 
   return (
-    <div className={styles.links}>
-      {links.map((link) => {
-        return (
-          <NavLink
-            key={link.title}
-            title={link.title}
-            path={link.path}
-          ></NavLink>
-        );
-        // return (<navLink title={link.title} path={link.path}}/>);
-      })}
-      {session ? (
-        <>
-          {isAdmin && <NavLink title="Admin" path="/admin"></NavLink>}
-          <button className={styles.auth_btn}>Logout</button>
-        </>
-      ) : (
-        <NavLink title="Login" path="/login"></NavLink>
+    <div className={styles.container}>
+      <div className={styles.links}>
+        {links.map((link) => {
+          return (
+            <NavLink
+              key={link.title}
+              title={link.title}
+              path={link.path}
+            ></NavLink>
+          );
+          // return (<navLink title={link.title} path={link.path}}/>);
+        })}
+        {session ? (
+          <>
+            {isAdmin && <NavLink title="Admin" path="/admin"></NavLink>}
+            <button className={styles.auth_btn}>Logout</button>
+          </>
+        ) : (
+          <NavLink title="Login" path="/login"></NavLink>
+        )}
+      </div>
+
+      <button
+        className={styles.menuButton}
+        onClick={() => setOpen((prev) => !prev)}
+      >
+        Menu
+      </button>
+      {open && (
+        <div className={styles.mobileLinks}>
+          {links.map((link) => {
+            return (
+              <NavLink
+                key={link.title}
+                title={link.title}
+                path={link.path}
+              ></NavLink>
+            );
+          })}
+        </div>
       )}
     </div>
   );
